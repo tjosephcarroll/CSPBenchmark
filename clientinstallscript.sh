aa3c1e124aed7d8edf5965cc7ea534fa7681503e
tjosephcarroll <jcarroll@jcarrollmbpr15.iscinternal.com>
2017-05-02 18:48:56 -0400

    5/2 adding wrk installation to the script

--- clientinstallscript.sh
+++ clientinstallscript.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 echo "Client Installation Script"
+sudo  yum groupinstall 'Development Tools'
+sudo yum install  openssl-devel
+sudo yum install  git
+git clone https://github.com/wg/wrk.git
+cd wrk
+make
