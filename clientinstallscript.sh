6624fe60231a0336ec6726e67d97a5fdb3751391
tjosephcarroll <jcarroll@jcarrollmbpr15.iscinternal.com>
2017-05-02 18:54:26 -0400

    5/2 added ruby install and pulling codebase from git

--- clientinstallscript.sh
+++ clientinstallscript.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
-echo "Client Installation Script"
+echo "Client Installation Script - RHEL"
+
+#wrk installation
 sudo  yum groupinstall 'Development Tools'
 sudo yum install  openssl-devel
 sudo yum install  git
 git clone https://github.com/wg/wrk.git
 cd wrk
 make
+cd ..
+
+#ruby installation
+sudo yum install ruby
+
+#get benchmark from git
+git clone https://github.com/tjosephcarroll/CSPBenchmark
+
+
