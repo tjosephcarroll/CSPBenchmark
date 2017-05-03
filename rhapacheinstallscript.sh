64b77deac3fc23fb884c5899d9f7e3014588be93
tjosephcarroll <tjosephcarroll@gmail.com>
2017-05-02 22:51:28 -0400

    5/2 update to rh apache script

--- rhapacheinstallscript.sh
+++ rhapacheinstallscript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #only for use on RHEL
-yum install -y -q httpd24-httpd
+sudo yum install -y -q httpd24-httpd
 
 #change to kitserver
 sudo ./kitserver/kits/released/Cache/2017.2.0/2017.2.0.651.0/kits/unix/2017.2.0.651.0/cinstall
