1530fd2f8935cc95af255714bb15e30a74c72adb
tjosephcarroll <tjosephcarroll@gmail.com>
2017-05-03 16:30:43 -0400

    Change-Id: I96240fe951a3f735d3daa8b188a3d238d33b1ec1

--- rhapacheinstallscript.sh
+++ rhapacheinstallscript.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 #only for use on RHEL
-sudo yum install -y -q httpd24-httpd
+sudo yum install -y -q httpd
 
 #change to kitserver
-sudo ./kitserver/kits/released/Cache/2017.2.0/2017.2.0.651.0/kits/unix/2017.2.0.651.0/cinstall
+cd /kitserver/kits/released/Cache/2017.2.0/2017.2.0.651.0/kits/unix/2017.2.0.651.0/
+
+#stop apache
+sudo service httpd stop
+
+#install CSP gateway (right now this is interactive)
+sudo ./cinstall
+
+#start apache
+sudo service httpd start
