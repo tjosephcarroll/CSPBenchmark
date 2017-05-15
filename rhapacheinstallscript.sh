6f5e1d05c6e71f0aed8f7095333fd27b7e035197
tjosephcarroll <tjosephcarroll@gmail.com>
2017-05-15 13:35:49 -0400

    add notes to fix apache upgrade problem. /run/httpd must exist

--- rhapacheinstallscript.sh
+++ rhapacheinstallscript.sh
@@ -2,6 +2,10 @@
 #only for use on RHEL
 sudo yum install -y -q httpd
 
+#make sure the /run/httpd is a real directory
+mkdir /run/httpd
+chmod 777 /run/httpd
+
 #change to kitserver
 cd /kitserver/kits/released/Cache/2017.2.0/2017.2.0.651.0/kits/unix/2017.2.0.651.0/
 
